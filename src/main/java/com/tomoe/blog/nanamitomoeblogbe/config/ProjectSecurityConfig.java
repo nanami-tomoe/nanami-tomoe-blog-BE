c8aec25b50c75b1e3404e9ae0659b6cebcf154e2
nanami-tomoe <npnp123npnp123@gmail.com>
2025-03-18 08:52:39 +0900

    feat: google 소셜 로그인 구현 - "/", "/mail/send" 요청은 모두 허용 - 리디렉션 경로는 /profile

--- src/main/java/com/tomoe/blog/nanamitomoeblogbe/config/ProjectSecurityConfig.java
+++ src/main/java/com/tomoe/blog/nanamitomoeblogbe/config/ProjectSecurityConfig.java
@@ -1,23 +1,24 @@
 package com.tomoe.blog.nanamitomoeblogbe.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers("/").permitAll();
-                    registry.anyRequest().authenticated();
-                })
-                .oauth2Login(oauth2login -> {
-                    oauth2login
-                            .successHandler((request, response, authentication) -> response.sendRedirect("/profile"));
-                })
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/mail/send").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/profile", true) // 로그인 후 무조건 /profile 로 이동
+                )
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
